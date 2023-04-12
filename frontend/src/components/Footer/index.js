ad30d8da2481cf7e0b6b4bbaf18c0af24c6746b3
ARGEEK1 <antonio198889@gmail.com>
2023-04-12 19:50:45 +0200

    add componet SocialIcons, icos and styles settings

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -1,5 +1,5 @@
 import ItemsContainer from "../ItemsContainer";
-
+import SocialIcons from "../SocialIcons";
 
 export default function Footer() {
     return (
@@ -16,6 +16,11 @@ export default function Footer() {
                 </div>
             </div>
             <ItemsContainer />
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 text-center pt-2 text-gray-400 text-sm pb-8">
+                <span>© 2020 Appy. All rights reserved.</span>
+                <span>Terms · Privacy Policy</span>
+                <SocialIcons />
+            </div>
         </footer>
     )
 }
