0090672ee3ac9be93ecc3f33c1c47bf74a76e361
Bryan Ismael Figueroa Quiroz <bryalquiroz@gmail.com>
2023-04-12 18:09:42 -0500

    🔁[UPDATE] Styles navbar pc

--- frontend/src/components/NavLink/index.jsx
+++ frontend/src/components/NavLink/index.jsx
@@ -16,9 +16,9 @@ export default function NavLink({
                     {text}
                 </Link>
                 {sonLinks && (
-                    <ul className="w-max absolute top-[calc(100%+4px)] rounded-lg scale-y-0 transition-all duration-200 origin-top group-hover:scale-y-100 bg-blue-200">
+                    <ul className="w-max py-4 px-2 absolute top-[calc(100%+4px)] rounded-lg scale-y-0 transition-all duration-200 origin-top group-hover:scale-y-100 bg-white drop-shadow-lg">
                         {sonLinks.map(({ text, linkSon }) => (
-                            <li className="px-1 relative before:transition-all before:duration-300 rounded-md before:content-[''] before:w-full before:scale-x-0 before:h-1 before:rounded-lg before:bg-white before:absolute before:top-full hover:before:scale-x-100 before:origin-bottom-left">
+                            <li className="px-1 relative before:transition-all before:duration-300 rounded-md before:content-[''] before:w-full before:scale-x-0 before:h-1 before:rounded-lg before:bg-blue-600 before:absolute before:top-full hover:before:scale-x-100 before:origin-bottom-left">
                                 <Link
                                     className={`font-bold text-lg ${color}`}
                                     href={linkSon}
