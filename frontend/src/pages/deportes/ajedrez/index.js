21a37cb9a0949ab5346879e0bdf1ab7434b9293d
DSinisterra <denissinisterra@gmail.com>
2023-04-12 14:22:01 -0500

    styles for chess

--- frontend/src/pages/deportes/ajedrez/index.js
+++ frontend/src/pages/deportes/ajedrez/index.js
@@ -2,25 +2,25 @@ import React from 'react';
 
 export default function Chess() {
   return (
-    <div>
-      <h1>Club De Ajedrez Zugzwang</h1>
+    <div className='max-w-2xl mx-auto'>
+      <h1 className='text-4xl font-bold text-gray-800 text-center p-5'>Club De Ajedrez Zugzwang</h1>
 
-      <img src="https://wallpaperaccess.com/full/8357317.jpg" alt="imagen de ajedrez" loading='lazy' />
+      <img src="https://wallpaperaccess.com/full/8357317.jpg" alt="imagen de ajedrez" loading='lazy' className='w-full rounded-full p-5' />
 
-      <p>La idea de club de ajedrez nació allá por el 2016 con una comisión directiva integrada por <em>Ezequiel Alfero, Jesuan Letizia, Gabriel Izak, Daniel Astaburuaga y Oscar Ponomareff</em>. Hoy es una realidad en el <strong>Club Agronomía Central</strong>, con sus torneos pensados, rápidos y blitz y tradicionales torneos por equipos, mixturado con una escuela de ajedrez comandada por la <em>maestra internacional y excampeona argentina Marisa Zuriel</em>, quien dicta las clases para nivel intermedio martes 18hs.</p>
-
-
-      <p>En un ambiente familiar y sumamente amistoso, se desarrollan los <strong>martes a las 19.30hs</strong> los certámenes de ritmo más lento, mientras que los <strong>sábados a las 16hs</strong>, están a disposición los de ritmo más rápido. Donde jugadores de todas las edades combinan <em>mates, jaques, celadas y todas las actividades relacionadas al deporte ciencia</em>.</p>
+      <p className='text-base text-gray-700 leading-6 px-8 py-2 mx-5 mt-3'>La idea de club de ajedrez nació allá por el 2016 con una comisión directiva integrada por <em>Ezequiel Alfero, Jesuan Letizia, Gabriel Izak, Daniel Astaburuaga y Oscar Ponomareff</em>. Hoy es una realidad en el <strong>Club Agronomía Central</strong>, con sus torneos pensados, rápidos y blitz y tradicionales torneos por equipos, mixturado con una escuela de ajedrez comandada por la <em>maestra internacional y excampeona argentina Marisa Zuriel</em>, quien dicta las clases para nivel intermedio martes 18hs.</p>
 
 
-      <p>Por otro lado, la escuela de ajedrez a nivel inicial para niños y juveniles, de la mano de <em>Horacio Taddeo</em>, gran referente del ajedrez escolar, se desarrolla los <strong>martes a las 18hs</strong> y a su vez, <em>Zuriel</em> lleva adelante en el mismo horario la escuela para nivel intermedio.</p>
+      <p className='text-base text-gray-700 leading-6 px-8 py-2 mx-5'>En un ambiente familiar y sumamente amistoso, se desarrollan los <strong>martes a las 19.30hs</strong> los certámenes de ritmo más lento, mientras que los <strong>sábados a las 16hs</strong>, están a disposición los de ritmo más rápido. Donde jugadores de todas las edades combinan <em>mates, jaques, celadas y todas las actividades relacionadas al deporte ciencia</em>.</p>
 
 
-      <p>El Club De Ajedrez Zugzwang en el año 2019 se afilio a la <strong>federación metropolitana de ajedrez</strong> para darle un formato legal y serio a su constante desarrollo.</p>
+      <p className='text-base text-gray-700 leading-6 px-8 py-2 mx-5'>Por otro lado, la escuela de ajedrez a nivel inicial para niños y juveniles, de la mano de <em>Horacio Taddeo</em>, gran referente del ajedrez escolar, se desarrolla los <strong>martes a las 18hs</strong> y a su vez, <em>Zuriel</em> lleva adelante en el mismo horario la escuela para nivel intermedio.</p>
 
-      <p>Si te gusta el ajedrez, este es el lugar ideal para estudiar, aprender, jugar y divertirse entre amigos.</p>
 
-      <h3>¡Los esperamos!</h3>
+      <p className='text-base text-gray-700 leading-6 px-8 py-2 mx-5'>El Club De Ajedrez Zugzwang en el año 2019 se afilio a la <strong>federación metropolitana de ajedrez</strong> para darle un formato legal y serio a su constante desarrollo.</p>
+
+      <p className='text-base text-gray-700 leading-6 px-8 py-2 mx-5'>Si te gusta el ajedrez, este es el lugar ideal para estudiar, aprender, jugar y divertirse entre amigos.</p>
+
+      <h3 className='text-lg font-semibold text-gray-800 text-center p-5'>¡Los esperamos!</h3>
 
     </div>
   )
