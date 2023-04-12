b3b2c9388bb36f35c7c01c4159be0c78ba803958
Bryan Ismael Figueroa Quiroz <bryalquiroz@gmail.com>
2023-04-12 16:36:07 -0500

    ✨[ADD] Menu Mobile

--- frontend/src/components/Navbar/index.jsx
+++ frontend/src/components/Navbar/index.jsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { useState } from 'react';
+import MobileMenu from '../MobileMenu';
 import NavLink from '../NavLink';
 
 export function Navbar() {
@@ -23,29 +24,32 @@ export function Navbar() {
                 <NavLink text={'HISTORIAS'} link={'/historias'} />
                 <NavLink text={'CONTACTANOS'} link={'/contacto'} />
             </ul>
-            <button
-                onClick={handleToggleMenu}
-                className="flex flex-col w-10 h-10 justify-center items-center border-none bg-transparent gap-[0.62rem] lg:hidden"
-            >
-                <div
-                    className={`w-full h-[5px] bg-black rounded-md transition-all duration-500 origin-left ${
-                        openMenu && 'rotate-45'
-                    }
+            <div className="w-10 h-10 relative">
+                <button
+                    onClick={handleToggleMenu}
+                    className="flex absolute z-50 flex-col w-10 h-10 justify-center items-center border-none bg-transparent gap-[0.62rem] lg:hidden"
+                >
+                    <div
+                        className={`w-full h-[5px] bg-black rounded-md transition-all duration-500 origin-left ${
+                            openMenu && 'rotate-45'
+                        }
                     `}
-                ></div>
-                <div
-                    className={`w-full h-[5px] bg-black rounded-md transition-all duration-300 origin-left ${
-                        openMenu && 'opacity-0 -translate-x-2'
-                    }
+                    ></div>
+                    <div
+                        className={`w-full h-[5px] bg-black rounded-md transition-all duration-300 origin-left ${
+                            openMenu && 'opacity-0 -translate-x-2'
+                        }
                     `}
-                ></div>
-                <div
-                    className={`w-full h-[5px] bg-black rounded-md transition-all duration-500 origin-left ${
-                        openMenu && '-rotate-45'
-                    }
+                    ></div>
+                    <div
+                        className={`w-full h-[5px] bg-black rounded-md transition-all duration-500 origin-left ${
+                            openMenu && '-rotate-45'
+                        }
                     `}
-                ></div>
-            </button>
+                    ></div>
+                </button>
+            </div>
+            {openMenu && <MobileMenu />}
         </header>
     );
 }
