755888ab42fe57c56c0b16dd53e371730e20134c
Elvis Franco <116116251+TecGato@users.noreply.github.com>
2023-04-12 10:51:36 -0400

    Add Pictures for History page

--- frontend/src/pages/club/historia/index.js
+++ frontend/src/pages/club/historia/index.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
+import HistoryPic1 from '../../../../public/HistoryPic1.jpg';
 
 export default function History() {
   return (
-    <div>History</div>
-  )
+    <div class='max-w-md mx-auto bg-white overflow-hidden md:max-w-2xl'>
+      <div class='md:flex'>
+        <h1>Algo de nuestra historia: </h1>
+      </div>
+    </div>
+  );
 }
