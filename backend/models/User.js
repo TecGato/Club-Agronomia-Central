44f1806bc6feb23ec24801aa8c434b7b6bb6779d
Bryan Ismael Figueroa Quiroz <bryalquiroz@gmail.com>
2023-04-12 18:38:35 -0500

    ✨[ADD] middleware validate JWT

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,39 +1,54 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    lowercase: true,
-    required: true,
-    validate: {
-      validator: (v) => /^S+@S+.S+$/.test(v),
-      message: (props) => `${props.value} no es una dirección de correo válida`,
+    name: {
+        type: String,
+        required: true,
     },
-  },
-  password: {
-    type: String,
-    validate: {
-      validator: (v) =>
-        /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/.test(
-          v
-        ),
-      message: (props) =>
-        `${props.value} no es una clave segura. Debe tener al menos 8 caracteres de largo, una minúscula, una mayúscula, un dígito y un caracter especial (- ! , *)`,
+    email: {
+        type: String,
+        lowercase: true,
+        required: true,
+        validate: {
+            validator: (v) => /^S+@S+.S+$/.test(v),
+            message: (props) =>
+                `${props.value} no es una dirección de correo válida`,
+        },
     },
-  },
-  createdAt: {
-    type: Date,
-    inmutable: true,
-    default: () => Date.now(),
-  },
-  updatedAt: {
-    type: Date,
-    default: () => Date.now(),
-  },
+    password: {
+        type: String,
+        validate: {
+            validator: (v) =>
+                /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/.test(
+                    v
+                ),
+            message: (props) =>
+                `${props.value} no es una clave segura. Debe tener al menos 8 caracteres de largo, una minúscula, una mayúscula, un dígito y un caracter especial (- ! , *)`,
+        },
+    },
+    google: {
+        type: Boolean,
+        default: false,
+    },
+    state: {
+        type: Boolean,
+        default: true,
+    },
+    createdAt: {
+        type: Date,
+        inmutable: true,
+        default: () => Date.now(),
+    },
+    updatedAt: {
+        type: Date,
+        default: () => Date.now(),
+    },
 });
+// convert _id to uid for better lecture
+userSchema.methos.toJSON = function () {
+    const { _id } = this.toObject();
+    user.uid = _id;
+    return user;
+};
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model('User', userSchema);
