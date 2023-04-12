9bbee2551657dc5b87245b80f46811ab986a644b
DSinisterra <denissinisterra@gmail.com>
2023-04-12 10:16:41 -0500

    Sports description and images upload

--- frontend/src/pages/deportes/futbol-infantil/index.js
+++ frontend/src/pages/deportes/futbol-infantil/index.js
@@ -2,6 +2,18 @@ import React from 'react'
 
 export default function FutInF() {
   return (
-    <div>FutInF</div>
+    <div>
+      <h1>Baby Futbol</h1>
+
+      <img src='https://img.freepik.com/free-vector/cartoon-football-player-collection_23-2149034182.jpg?size=626&ext=jpg' alt='futbol infantil' loading='lazy' />
+
+      <p>Por más de 40 años el club compite en baby futbol, en los primeros años en liga argentina, pasando por FAFI y actualmente en FEFI.</p>
+      <p>Este deporte se realiza con chicos que van <strong>desde los 6 años hasta los 13 años</strong>, que encuentren en el mismo, un sano espacio de recreación e iniciación en el deporte, como así también, medición de aptitudes deportivas contra otros niños de similar edad.</p>
+      <p>El baby futbol es una actividad tanto a niños como a familiares que disfrutan de hermosas jornadas viendo crecer y divertir a sus seres queridos.</p>
+      <p>El club en estos momentos cuenta con <strong>tres tiras</strong>, selectivo matutino, tira color y tira letra.</p>
+      <p>Las tiras selectivo matutino y color, practican los días martes y viernes. <strong>De 18.00hs a 19.00</strong> las categorías 2017/2016/2015, <strong>de 19.00hs a 20.00hs</strong> las categorías 2014/2013 y <strong>de 20.00hs a 21.00hs</strong> las categorías 2012/2011/2010, las mismas son dictadas por los profes <em>Diego, Gonzalo, Román, Lucas</em></p>
+      <p>La tira de letra practica los días <strong>lunes y miércoles de 17.00hs a 18.00hs</strong> categorías 2016/2015, <strong>de 18.00hs a 19.00hs</strong> categorías 2014/2013 y <strong>de 19.00hs a 20.00hs</strong> las categorías 2012/2011/2010, con los profes <em>Juan y Julián</em>.</p>
+
+    </div>
   )
 }
---
+++ frontend/src/pages/deportes/futsal-femenino/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function FutFem() {
+  return (
+    <div>
+      <h1>Futsal Femenino</h1> 
+
+      <img  src='https://img.freepik.com/premium-photo/fit-football-sportswear-doing-tricks_23-2148298897.jpg?size=626&ext=jpg&ga=GA1.1.2070745046.1681311087&semt=robertav1_2_sidr' alt='futsal femenino' loading='lazy' />
+      <p>El futsal femenino se practica en nuestro club desde el año 2016, saliendo <strong>campeones en varias oportunidades</strong> en la actualidad.</p> 
+      
+      <p>Contamos con 1 division, unica, 3 division y 4 division, compitiendo en la <strong>Liga Laamba</strong>, el profe a cargo es <em>Fernando</em> y los dias de practica son <strong>lunes y miercoles de 20.00hs a 22.00hs</strong> todas las categorias.</p>
+
+    </div>
+  )
+}
