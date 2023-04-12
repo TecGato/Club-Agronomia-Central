c0ffec029ad23455f813831d7a4824e3bb14f819
Bryan Ismael Figueroa Quiroz <bryalquiroz@gmail.com>
2023-04-11 20:40:16 -0500

    ✨[ADD] Principal links navbar pc

--- frontend/src/components/Navbar/index.jsx
+++ frontend/src/components/Navbar/index.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { useState } from 'react';
 
 export function Navbar() {
@@ -10,8 +11,37 @@ export function Navbar() {
     return (
         <header className="w-full h-14 flex justify-between items-center outline outline-1 outline-red-700 px-3">
             <img src="logo.webp" className="w-12" alt="" />
-            <p className="font-semibold text-lg">Club Agronomia Central</p>
+            <p className="font-semibold text-lg lg:hidden">
+                Club Agronomia Central
+            </p>
 
+            <ul className="gap-4 hidden lg:flex">
+                <li className="hover:bg-blue-700 px-1 transition-all duration-300 rounded-md">
+                    <Link className="font-bold text-lg" href="/">
+                        INICIO
+                    </Link>
+                </li>
+                <li className="hover:bg-blue-700 px-1 transition-all duration-300 rounded-md">
+                    <Link className="font-bold text-lg" href="/club">
+                        EL CLUB
+                    </Link>
+                </li>
+                <li className="hover:bg-blue-700 px-1 transition-all duration-300 rounded-md">
+                    <Link className="font-bold text-lg" href="/deportes">
+                        DEPORTES
+                    </Link>
+                </li>
+                <li className="hover:bg-blue-700 px-1 transition-all duration-300 rounded-md">
+                    <Link className="font-bold text-lg" href="/historias">
+                        HISTORIAS
+                    </Link>
+                </li>
+                <li className="hover:bg-blue-700 px-1 transition-all duration-300 rounded-md">
+                    <Link className="font-bold text-lg" href="/contacto">
+                        CONTACTANOS
+                    </Link>
+                </li>
+            </ul>
             <button
                 onClick={handleToggleMenu}
                 className="flex flex-col w-10 h-10 justify-center items-center border-none bg-transparent gap-[0.62rem] lg:hidden"
