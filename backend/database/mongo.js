99170033904c12c38ec007b49a9fd15eb7aff59d
Javier Diaz <javiermauriciodiaz@gmail.com>
2023-04-12 22:19:28 -0500

    create connection file with the string connection to export.

---
+++ backend/database/mongo.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+
+const connectionDB = ()=>{
+    const connectionString = `mongodb+srv://henrystudents:henrypf35a@mongo.dau0olv.mongodb.net/test`    
+    mongoose.connect(connectionString)
+        .then(() => {
+            console.log('connected')
+        }).catch(err => console.log(err))
+}
+
+module.exports = connectionDB
