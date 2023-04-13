61dfcb71818bd82633658825a6abd7666dce0032
Luis Landaeta <luis.landaeta24@gmail.com>
2023-04-13 12:21:27 -0400

    Helpers can now take out data from db

--- backend/helpers/posts/posts.js
+++ backend/helpers/posts/posts.js
@@ -1,15 +1,18 @@
+const mongoose = require("mongoose");
+const Post = require("../../models/Post");
+
 const getAllPosts = async () => {
   try {
-    const allPosts = await "CREAR MODELO Y CONECTAR";
+    const allPosts = await Post.find();
     return allPosts;
   } catch (error) {
     console.error(error.message);
   }
 };
 
-const getPostByName = async (name) => {
+const getPostByTitle = async (title) => {
   try {
-    const post = await "CREAR MODELO Y CONECTAR";
+    const post = await Post.findOne({ title: title });
     return post;
   } catch (error) {
     console.error(error.message);
@@ -18,11 +21,11 @@ const getPostByName = async (name) => {
 
 const getPostById = async (id) => {
   try {
-    const post = await "CREAR MODELO Y CONECTAR";
+    const post = await Post.findById(id);
     return post;
   } catch (error) {
     console.error(error.message);
   }
 };
 
-module.exports = { getAllPosts, getPostByName, getPostById };
+module.exports = { getAllPosts, getPostByTitle, getPostById };
