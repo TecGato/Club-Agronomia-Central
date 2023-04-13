6d1898ff14a935817e32be65af07c468f9fa671b
ARGEEK1 <antonio198889@gmail.com>
2023-04-14 01:10:39 +0200

    [ADD] file images.js in page cetro-entranamiento

--- frontend/src/pages/deportes/centro-de-entrenamiento/images.js
+++ frontend/src/pages/deportes/centro-de-entrenamiento/images.js
@@ -1,6 +1,9 @@
 export const images = [
-    {src: "sports-img/ajedrez.jpg"},
-    {src: "sports-img/futbol.jpg"},
-    {src: "sports-img/futsal.jpg"},
-    {src: "sports-img/taekwondo.jpg"},
+    {src: "/sports-img/Atenea1.jpg"},
+    {src: "/sports-img/Atenea2.jpg"},
+    {src: "/sports-img/Atenea3.jpg"},
+    {src: "/sports-img/Atenea4.jpg"},
+    {src: "/sports-img/Atenea5.jpg"},
+    {src: "/sports-img/Atenea6.jpg"},
+    
 ];
