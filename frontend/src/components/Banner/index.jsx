7c0d77671d5866ab762cc6b845e335c243e62836
ARGEEK1 <antonio198889@gmail.com>
2023-04-13 16:46:17 +0200

    [ADD] Arrow for the banner

--- frontend/src/components/Banner/index.jsx
+++ frontend/src/components/Banner/index.jsx
@@ -1,12 +1,20 @@
-import { slides } from "./slides"
+import { slides, arrow } from "./slides"
 
 export function Banner() {
     return (
-        <div className="max-w-[100%] h-[720px] w-full m-auto relative">
-            <div style={{backgroundImage:`url(${slides[3].src})`}} className="w-full h-full rounded-2xl bg-center bg-cover duration-500">
-               
+        <div className="max-w-[100%] h-[500px] w-full m-auto relative">
+            <div style={{backgroundImage:`url(${slides[0].src})`}} className="w-full h-full rounded-2xl bg-center bg-cover duration-500">
             </div>
-            
+            <div className="absolute top-[50%] -translate-x-0 tranlate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+                {
+                    arrow[0].svg
+                }
+            </div>
+            <div className="absolute top-[50%] -translate-x-0 tranlate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+                {
+                    arrow[1].svg
+                }
+            </div>
         </div>
     );
 };
