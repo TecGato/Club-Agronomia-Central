eba512df86849b813729217ba58df7f10d55fcef
Bryan Ismael Figueroa Quiroz <bryalquiroz@gmail.com>
2023-04-13 13:26:09 -0500

    ✨[ADD] Routes post Event

--- backend/routes/events.js
+++ backend/routes/events.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 const { getEvents, getEventById } = require('../controllers/Events/getEvents');
+const { postEvent } = require('../controllers/Events/postEvents');
 
 const router = Router();
 
 router.get('/', getEvents);
 router.get('/', getEventById);
+router.post('/', postEvent);
 
 module.exports = router;
