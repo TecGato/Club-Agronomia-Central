2b88993839f1b92199598264cc86cd777811ad24
ARGEEK1 <antonio198889@gmail.com>
2023-04-13 10:53:02 +0200

    Add elements for redirection into the app

--- frontend/src/components/ItemsContainer/index.jsx
+++ frontend/src/components/ItemsContainer/index.jsx
@@ -1,11 +1,11 @@
 import Item from "../Item";
-import { PRODUCTS, RESOURCES, COMPANY, SUPPORT } from "./menus"
+import { EL_CLUB, DEPORTES, COMPANY, SUPPORT } from "./menus"
 
 export default function ItemsContainer() {
     return (
         <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-6 sm:px-8 px-5 py-16  ">
-            <Item Links={PRODUCTS} title="PRODUCTS"/>
-            <Item Links={RESOURCES} title="RESOURCES"/>
+            <Item Links={EL_CLUB} title="EL CLUB"/>
+            <Item Links={DEPORTES} title="DEPORTES"/>
             <Item Links={COMPANY} title="COMPANY"/>
             <Item Links={SUPPORT} title="SUPPORT"/>
         </div>
