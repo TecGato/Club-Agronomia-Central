a42eb8c0fdd04cfdd5331476a60c97892985e66f
ARGEEK1 <antonio198889@gmail.com>
2023-04-13 10:16:28 +0200

    Add anchor tags for social networks

--- frontend/src/components/SocialIcons/index.jsx
+++ frontend/src/components/SocialIcons/index.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import {Icons} from "./icons"
 
 export default function SocialIcons() {
@@ -5,7 +6,9 @@ export default function SocialIcons() {
         <div className="text-teal-500">
             {
                 Icons.map((icon) => (
-                    <span key={icon.name} className="p-2  cursor-pointer inline-flex items-center rounded-full bg-gray-700 mx-1.5 text-xl hover:text-gray-100 hover:bg-gray-100 duration">{icon.svg}</span>
+                    <a href={icon.link} target="_blank">
+                        <span key={icon.name} className="p-2  cursor-pointer inline-flex items-center rounded-full bg-gray-700 mx-1.5 text-xl hover:text-gray-100 hover:bg-gray-100 duration">{icon.svg}</span>
+                    </a>
                 ))
             }
         </div>
