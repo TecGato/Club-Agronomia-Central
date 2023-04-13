8614ec5daf0529f76003bec85762066f0d36f611
DSinisterra <denissinisterra@gmail.com>
2023-04-13 12:21:20 -0500

    styles futsal femenino

--- frontend/src/pages/deportes/futbol-infantil/index.js
+++ frontend/src/pages/deportes/futbol-infantil/index.js
@@ -2,28 +2,6 @@ import React from 'react'
 
 export default function FutInF() {
   return (
-    // <div className='m-4'>
-    //   <h1 className='text-4xl font-bold text-gray-800 text-center p-5 m-3'>Baby Futbol</h1>
-
-    //   <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 place-items-center">
-
-    //     <div className='flex justify-center items-center bg-teal-100 rounded-full'>
-    //       <p className='text-base text-gray-700 leading-6 p-7 m-5 md:p-5 lg:mx-8 xl:p-8 xl:m-8 2xl:px-24 2xl:mx-12'>Por más de 40 años el club compite en baby futbol, en los primeros años en liga argentina, pasando por FAFI y actualmente en FEFI. El baby futbol es una actividad tanto a niños como a familiares que disfrutan de hermosas jornadas viendo crecer y divertir a sus seres queridos.</p>
-    //     </div>
-    //     <img src='https://img.freepik.com/vector-gratis/pack-futbolista-dibujos-animados_23-2149034184.jpg?w=740&t=st=1681342606~exp=1681343206~hmac=03db0b6bcd6a97fcd2e74a1446b1f54ef3d87f5f2d9cb4c76584dcda9b678504' alt='futbol infantil' loading='lazy' className='w-full' />
-    //     <div className='flex justify-center items-center bg-teal-100 rounded-full'>
-    //       <p className='text-base text-gray-700 leading-6 p-7 m-5  md:p-5 lg:mx-8 xl:p-8 xl:m-8 2xl:px-24 2xl:mx-12'>Este deporte se realiza con chicos que van <strong>desde los 6 años hasta los 13 años</strong>, que encuentren en el mismo, un sano espacio de recreación e iniciación en el deporte, como así también, medición de aptitudes deportivas contra otros niños de similar edad.</p>
-    //     </div>
-    //     <div className='flex justify-center items-center bg-teal-100 rounded-full'>
-    //       <p className='text-base text-gray-700 leading-6 p-7 m-5  md:p-5 lg:mx-8 xl:p-8 xl:m-8 2xl:px-24 2xl:mx-12'>Las tiras selectivo matutino y color, practican los días martes y viernes. <strong>De 18.00hs a 19.00</strong> las categorías 2017/2016/2015, <strong>de 19.00hs a 20.00hs</strong> las categorías 2014/2013 y <strong>de 20.00hs a 21.00hs</strong> las categorías 2012/2011/2010, las mismas son dictadas por los profes <em>Diego, Gonzalo, Román, Lucas</em></p>
-    //     </div>
-    //     <img src='https://img.freepik.com/vector-gratis/jugadores-futbol-dibujos-animados_23-2149034181.jpg?size=626&ext=jpg&ga=GA1.2.1228025790.1681327921&semt=ais' alt='futbol infantil' loading='lazy' className='w-full' />
-    //     <div className='flex justify-center items-center bg-teal-100 rounded-full'> 
-    //       <p className='text-base text-gray-700 leading-6 p-7 m-5  md:p-5 lg:mx-8 xl:p-8 xl:m-8 2xl:px-24 2xl:mx-12'>La tira de letra practica los días <strong>lunes y miércoles de 17.00hs a 18.00hs</strong> categorías 2016/2015, <strong>de 18.00hs a 19.00hs</strong> categorías 2014/2013 y <strong>de 19.00hs a 20.00hs</strong> las categorías 2012/2011/2010, con los profes <em>Juan y Julián</em>.</p>
-    //     </div>
-    //   </div>
-    // </div>
-
     <div className='m-4'>
     <h1 className='text-4xl font-bold text-gray-800 text-center p-5 m-3'>Baby Futbol</h1>
     
--- frontend/src/pages/deportes/futsal-femenino/index.js
+++ frontend/src/pages/deportes/futsal-femenino/index.js
@@ -2,14 +2,37 @@ import React from 'react'
 
 export default function FutFem() {
   return (
-    <div>
-      <h1>Futsal Femenino</h1> 
+    <div className='m-4'>
+      <h1 className='text-4xl font-bold text-gray-800 text-center p-5 m-3'>Futsal Femenino</h1> 
 
-      <img  src='https://img.freepik.com/premium-photo/fit-football-sportswear-doing-tricks_23-2148298897.jpg?size=626&ext=jpg&ga=GA1.1.2070745046.1681311087&semt=robertav1_2_sidr' alt='futsal femenino' loading='lazy' />
-      <p>El futsal femenino se practica en nuestro club desde el año 2016, saliendo <strong>campeones en varias oportunidades</strong> en la actualidad.</p> 
-      
-      <p>Contamos con 1 division, unica, 3 division y 4 division, compitiendo en la <strong>Liga Laamba</strong>, el profe a cargo es <em>Fernando</em> y los dias de practica son <strong>lunes y miercoles de 20.00hs a 22.00hs</strong> todas las categorias.</p>
+      <div className='grid grid-cols-1 md:grid-row-2 md:grid-cols-3 place-items-center'>
 
+        <div className='row-span-1 p-8 grid grid-cols-4 grid-row-2'>
+          <h3 className='text-lg text-center font-bold text-gray-800 text-center p-5 row-span-1 col-span-3'>En el Club</h3>
+          <p className='text-base text-gray-700 leading-6 py-0 md:text-right row-span-1 col-span-3'>Se practica en nuestro club desde el año 2016</p>
+          <img src='https://cdn.icon-icons.com/icons2/1036/PNG/96/Soccer_Ball_icon-icons.com_76265.png' alt='corona' className='w-8 h-8 row-span-2 col-span-1 place-self-center' />
+        </div>
+
+        <img  src='https://img.freepik.com/foto-gratis/mujer-atletica-pateando-futbol_23-2148298864.jpg?size=626&ext=jpg&ga=GA1.1.1228025790.1681327921&semt=ais' alt='futsal femenino' loading='lazy' className='shadow-xl row-span-2' />
+
+        <div className='row-span-1 p-8 grid grid-cols-4 grid-row-2'>
+          <img src='https://cdn.icon-icons.com/icons2/390/PNG/96/queen-crown_38957.png' alt='corona' className='w-8 h-8 row-span-2 col-span-1 place-self-center' />
+          <h3 className='text-lg text-center font-bold text-gray-800 text-center p-5 row-span-1 col-span-3'>Campeonas</h3>  
+          <p className='text-base text-gray-700 leading-6 py-0 row-span-1 col-span-3'>En varias oportunidades, en la actualidad</p> 
+        </div>
+
+        <div className='row-span-1 p-8 grid grid-cols-4 grid-row-2'>
+          <img src='https://cdn.icon-icons.com/icons2/3069/PNG/96/office_trophy_winner_competition_reward_icon_190988.png' alt='liga' className='w-8 h-8 row-span-2 col-span-1 place-self-center md:order-3' />
+          <h3 className='text-lg text-center font-bold text-gray-800 text-center p-5 row-span-1 col-span-3 md:order-1'>Liga Laamba</h3>  
+          <p className='text-base text-gray-700 leading-6 py-0 md:text-right row-span-1 col-span-3 md:order-2'>Contamos con 1 division, unica, 3 division y 4 division compitiendo en esta liga</p>
+        </div>
+
+        <div className='row-span-1 p-8 grid grid-cols-4 grid-row-2'>
+          <img src='https://cdn.icon-icons.com/icons2/67/PNG/96/soccersports_futbo_13301.png' alt='practica' className='w-8 h-8 row-span-2 col-span-1 place-self-center' />
+          <h3 className='text-lg text-center font-bold text-gray-800 text-center p-5 row-span-1 col-span-3'>Practica</h3>  
+          <p className='text-base text-gray-700 leading-6 py-0 row-span-1 col-span-3'>El profe a cargo es <em>Fernando</em> y las practicas: <strong>lunes y miercoles de 20.00hs a 22.00hs</strong> todas las categorias.</p>
+        </div>
+      </div>
     </div>
   )
 }
