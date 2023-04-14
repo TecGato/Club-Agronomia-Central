69ce6372f46e9cc192440dbaca575b6dacfa6bff
ARGEEK1 <antonio198889@gmail.com>
2023-04-14 20:07:31 +0200

    [ADD] file taekwondo.js in folder taekwondo

---
+++ frontend/src/pages/deportes/taekwondo/taekwondo.js
@@ -0,0 +1,14 @@
+export const images = [
+    {src: "/sports-img/Taekwondo1.jpg"},
+    {src: "/sports-img/Taekwondo2.jpg"},
+    {src: "/sports-img/Taekwondo3.jpg"},
+    {src: "/sports-img/Taekwondo4.jpg"},
+    {src: "/sports-img/Taekwondo5.jpg"},
+    {src: "/sports-img/Taekwondo6.jpg"},
+    
+];
+
+export const info = {
+    title: "Taekwondo",
+    description: "<p>Una nueva actividad se inicia este año y es taekwondo, las clases son dictadas por el profesor <em>Carlos Tonio</em>, 2 dan de taekwondo ITF, con una trayectoria de más de 30 años practicando el deporte y 7 años de experiencia en la docencia deportiva.</p><p>Te invitamos a que seas parte de nuestra actividad los días <strong>miércoles y viernes de 19.30hs a 20.30hs</strong> a partir de los 7 años y los mismos días <strong>de 20.30hs a 22.00hs</strong> juveniles mayores de 14 años y adultos.</p> <h3>¡Primeras clases gratis!</h3>",
+};
