b91ef295f7f4239becdd9cdcdbdc03a6359673f0
ARGEEK1 <antonio198889@gmail.com>
2023-04-14 22:29:47 +0200

    [UPDATE] page taekwondo

--- frontend/src/pages/deportes/taekwondo/index.jsx
+++ frontend/src/pages/deportes/taekwondo/index.jsx
@@ -6,20 +6,9 @@ import { images, info } from './taekwondo';
 
 export default function Taekwondo() {
   return (
-
     <Layout>
       <HeaderTitle image={images[4].src} title="TAEKWONDO" />
-      
+      <InfoSlider images={images} info={info}/>
     </Layout>
-    // <div>
-
-
-    //   <h1>Taekwondo</h1>
-
-    //   <p>Una nueva actividad se inicia este año y es taekwondo, las clases son dictadas por el profesor <em>Carlos Tonio</em>, 2 dan de taekwondo ITF, con una trayectoria de más de 30 años practicando el deporte y 7 años de experiencia en la docencia deportiva.</p>
-    //   <p>Te invitamos a que seas parte de nuestra actividad los días <strong>miércoles y viernes de 19.30hs a 20.30hs</strong> a partir de los 7 años y los mismos días <strong>de 20.30hs a 22.00hs</strong> juveniles mayores de 14 años y adultos.</p> 
-    //   <h3>¡Primeras clases gratis!</h3>
-
-    // </div>
   );
 };
