a1ee14630d6484f03aebbc54fbabed5110af4d58
ARGEEK1 <antonio198889@gmail.com>
2023-04-14 14:48:39 +0200

    [UPDATE] page centro de entrenamiento

--- frontend/src/pages/deportes/centro-de-entrenamiento/index.jsx
+++ frontend/src/pages/deportes/centro-de-entrenamiento/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import { HeaderTitle } from '@/components/HeaderTitle';
+import { InfoSlider } from '@/components/InfoSlider';
 import { images } from './images'
 
 
@@ -9,15 +10,7 @@ export default function TrainingCenter() {
   return (
     <div>
       <HeaderTitle image={images[3].src} title='ATENEA SPORTS' />
-      <h1>Atenea Sports</h1>
-
-      <h3>Centro de entrenamiento y readaptación deportiva.</h3>
-
-      <p>Atenea Sports es un espacio dedicado al entrenamiento, la readaptación deportiva y la rehabilitación de lesiones.</p>
-
-      <p>Desde una concepción integral y funcional de la persona, nuestro objetivo es ayudar a la persona a alcanzar su mejor versión, teniendo en cuenta sus objetivos, posibilidades y necesidades.</p>
-
-      <p>Cada sesión, cada trabajo y cada ejercicio está pensado por nuestros profesionales para que vivas el deporte, el ejercicio y la actividad física con la misma calidad que lo hacen los profesionales.</p>
+      <InfoSlider images={images} />
 
       <em>#EntrenaDistinto #RehabilitateMejor</em>
       <br />
@@ -25,7 +18,6 @@ export default function TrainingCenter() {
       <a href='https://instagram.com/ateneasports'>
         <Image src="/instagram-logo.svg" alt="Logo de Instagram" width={30} height={30} />
       </a>
-
     </div>
-  )
-}
+  );
+};
