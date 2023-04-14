c6fefef0dd4dac58e2fe3cfe8a2f443d58ebc4a2
ARGEEK1 <antonio198889@gmail.com>
2023-04-14 17:06:20 +0200

    [ADD] component Layout to page centro-entramiento

--- frontend/src/pages/deportes/centro-de-entrenamiento/index.jsx
+++ frontend/src/pages/deportes/centro-de-entrenamiento/index.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 import Image from 'next/image';
 import { HeaderTitle } from '@/components/HeaderTitle';
 import { InfoSlider } from '@/components/InfoSlider';
+import { Layout } from "@/components";
 import { images } from './images'
 
-
 export default function TrainingCenter() {
-  
   return (
-    <div>
+    <Layout>
       <HeaderTitle image={images[3].src} title='ATENEA SPORTS' />
       <InfoSlider images={images} />
-    </div>
+    </Layout>
   );
 };
