b76dfbc429d198945c92e4e857901028ae9f8f2e
JosueMoora <josuedavidmora99@gmail.com>
2023-04-15 13:25:01 -0400

    [Add] Club page

--- frontend/src/pages/club/comision-directiva/index.js
+++ frontend/src/pages/club/comision-directiva/index.js
@@ -19,14 +19,14 @@ export default function SteeringCommittee() {
     title={'Comisión Directiva'}
     image={'https://img.freepik.com/foto-gratis/reunion-departamento_23-2147626530.jpg?w=996&t=st=1681513108~exp=1681513708~hmac=eb17a71ffb76874c81f1199abbfe9cef4f0b1e4e4b7c07ea8c72ea9cc0604b0e'}
     >
-      <div className="lg:grid grid-cols-4 gap-8 py-10 px-5 ">
+      <div className="lg:grid grid-cols-4 gap-8 py-10 px-5 lg:px-0 ">
         {directiva.map(([cargo, nombre, image]) => (
           <div class=" max-w-sm mx-auto bg-white shadow-lg mb-10  ">
             <Image
               src={image}
               alt={nombre}
             />
-            <div class="text-center py-4 lg:text-left bg-gray-100">
+            <div class="text-center py-4 lg:text-left lg: pl-2 bg-gray-100">
               <p class="text-lg pb-3 text-black font-bold">{nombre}</p>
               <p class="text-slate-500 font-medium">{cargo}</p>
             </div>
