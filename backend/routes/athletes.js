7f8312815805bb39993acb5663fdbe3c6ee14cef
Luis Landaeta <luis.landaeta24@gmail.com>
2023-04-14 23:37:08 -0400

    get routes for Athletes created

--- backend/routes/athletes.js
+++ backend/routes/athletes.js
@@ -1,10 +1,9 @@
 const { Router } = require("express");
+const { getAthletes, getById } = require("../controllers/athletes/getAthletes");
 
 const router = Router();
 
 router.get("/", getAthletes);
-router.get("/", getByDiscipline);
-router.get("/", getByName);
-router.get("/", getById);
+router.get("/:id", getById);
 
 module.exports = router;
