988d52e4165e2f34c598102a542a4d114d6c3838
Bryan Ismael Figueroa Quiroz <bryalquiroz@gmail.com>
2023-04-14 21:56:52 -0500

    🛠[FIX] Styles Navbar

--- frontend/src/components/Dashboard/NavLink/index.jsx
+++ frontend/src/components/Dashboard/NavLink/index.jsx
@@ -5,9 +5,9 @@ export const NavLink = ({ icon, pathname, name }) => {
     const router = useRouter();
     return (
         <div
-            className={`w-full h-12 flex gap-4 items-center rounded-lg px-2 group cursor-pointer ${
+            className={`w-full h-12 flex gap-4 items-center rounded-lg px-2 group cursor-pointer transition-all duration-500 ${
                 router.pathname === pathname
-                    ? 'bg-blue-800'
+                    ? 'bg-[#1b418a]'
                     : 'hover:bg-neutral-800'
             } `}
         >
--- frontend/src/components/Dashboard/Navbar/index.jsx
+++ frontend/src/components/Dashboard/Navbar/index.jsx
@@ -5,13 +5,20 @@ export const Navbar = () => {
     return (
         <nav className="w-60 h-screen bg-neutral-900 flex flex-col justify-between items-center gap-1 py-7 px-4">
             <div>
-                <span>
+                <div className="flex gap-2 mb-5">
                     <img className="w-11" src="logo.webp" alt="" />
-                    <p>Club Agronomia Central</p>
-                </span>
-                {routes.map(({ icon, pathName, name }) => (
-                    <NavLink key={pathName} pathname={pathName} name={name} icon={icon} />
-                ))}
+                    <p className="text-white">Club Agronomia Central</p>
+                </div>
+                <div className="flex flex-col gap-1">
+                    {routes.map(({ icon, pathName, name }) => (
+                        <NavLink
+                            key={pathName}
+                            pathname={pathName}
+                            name={name}
+                            icon={icon}
+                        />
+                    ))}
+                </div>
             </div>
             <div>
                 <button>Cerrar Sesión</button>
