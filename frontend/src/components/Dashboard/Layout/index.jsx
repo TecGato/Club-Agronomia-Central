a1eb4a40e73ca0bcf49b2cfbf5c1fc9e7c02011f
Bryan Ismael Figueroa Quiroz <bryalquiroz@gmail.com>
2023-04-16 16:35:48 -0500

    ✨[ADD] Added Chart Js library

--- frontend/src/components/Dashboard/Layout/index.jsx
+++ frontend/src/components/Dashboard/Layout/index.jsx
@@ -2,7 +2,7 @@ import { Navbar } from '../Navbar';
 
 export const Layout = ({ children }) => {
     return (
-        <main className="grid grid-cols-1 lg:grid-cols-[15rem_1fr] bg-[#f9fafb]">
+        <main className="grid grid-cols-1 lg:grid-cols-[15rem_1fr] bg-[#f1f5f9]">
             <Navbar />
             {children}
         </main>
