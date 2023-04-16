5f4d609b18b4b6eb9339ab29f9e4f1cdf5352d40
Luis Landaeta <luis.landaeta24@gmail.com>
2023-04-16 13:34:01 -0400

    put routes updated

--- backend/helpers/athletes/putAthletes.js
+++ backend/helpers/athletes/putAthletes.js
@@ -2,7 +2,7 @@ const Athlete = require("../../models/Athlete");
 
 const putAthlete = async (id, athleteInfo) => {
   try {
-    const athlete = await Athlete.updateOne(id, { ...athleteInfo });
+    const athlete = await Athlete.findByIdAndUpdate(id, { ...athleteInfo });
     return athlete;
   } catch (error) {
     console.error(error.message);
