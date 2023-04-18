b26a4a5138a7f2874629c66756e91227207e58e4
AndresSalazar <andres.f.salazar@correounivalle.edu.co>
2023-04-18 09:44:53 -0500

    review

--- frontend/src/pages/dashboard/quincho/index.jsx
+++ frontend/src/pages/dashboard/quincho/index.jsx
@@ -1,10 +1,32 @@
 import { Layout } from '@/components/Dashboard';
 import { QuinchoCalendarContainer } from '@/components/Page';
 
+
+
+
 export default function Quincho() {
     return (
         <Layout>
             <div>index</div>
         </Layout>
     );
+}
+
+
+
+
+export async function getStaticProps(){
+    try{
+
+        const res = await fetch('http://localhost:3001/api/events');
+        const data = await res.json();
+
+        return {
+            props: { data }
+        }
+
+    }catch(error){
+        return{error:error.message}
+    }
+
 }
