1d73a40e1a792fed6adc668ffee637f47776f116
Luis Landaeta <luis.landaeta24@gmail.com>
2023-04-18 12:06:36 -0400

    don't remember what I changed but I'm sure it's fine

--- backend/routes/matches.js
+++ backend/routes/matches.js
@@ -1,12 +1,12 @@
-const { Router } = require('express');
-const { getMatches } = require('../controllers/matches/getMatches');
-const { createMatch } = require('../controllers/matches/postMatches');
-const { updateMatch } = require('../controllers/matches/putMatches');
+const { Router } = require("express");
+const { getMatches } = require("../controllers/matches/getMatches");
+const { createMatch } = require("../controllers/matches/postMatches");
+const { updateMatch } = require("../controllers/matches/putMatches");
 
 const router = Router();
 
-router.get('/', getMatches);
-router.post('/', createMatch);
-router.put('/:id', updateMatch);
+router.get("/", getMatches);
+router.post("/", createMatch);
+router.put("/:id", updateMatch);
 
 module.exports = router;
