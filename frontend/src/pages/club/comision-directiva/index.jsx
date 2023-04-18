ff8cae95f2cd7738a6036a13b63f2f8ac9c3d77a
JosueMoora <josuedavidmora99@gmail.com>
2023-04-18 15:40:01 -0400

    [Modified] Directives page

--- frontend/src/pages/club/comision-directiva/index.jsx
+++ frontend/src/pages/club/comision-directiva/index.jsx
@@ -4,7 +4,6 @@ import axios from "axios";
 import { Layout } from "@/components/Page";
 import image from "../../../../public/directives-img/directive.svg";
 
-
 export default function SteeringCommittee() {
   const [directives, setDirectives] = useState();
   useEffect(() => {
@@ -13,28 +12,28 @@ export default function SteeringCommittee() {
     });
   }, [directives]);
 
-    // const directives = [
-    //     {
-    //       name:'Diego Dottore',
-    //       position:'Presidente',
-    //   },
-    //     {
-    //       position:'Vicepresidente',
-    //       name:'Hernán Blotta',
-    //   },
-    //     {
-    //       position:'Secretario',
-    //       name:'Marcelo Vitar',
-    //   },
-    //     {
-    //       position: 'Pro-Secretario',
-    //       name: 'Gustavo Majluf',
-    //   },
-    //     {
-    //       position: 'Tesorero',
-    //       name: 'Pablo Jorge',
-    //   },
-    // ];
+  // const directives = [
+  //     {
+  //       name:'Diego Dottore',
+  //       position:'Presidente',
+  //   },
+  //     {
+  //       position:'Vicepresidente',
+  //       name:'Hernán Blotta',
+  //   },
+  //     {
+  //       position:'Secretario',
+  //       name:'Marcelo Vitar',
+  //   },
+  //     {
+  //       position: 'Pro-Secretario',
+  //       name: 'Gustavo Majluf',
+  //   },
+  //     {
+  //       position: 'Tesorero',
+  //       name: 'Pablo Jorge',
+  //   },
+  // ];
 
   return (
     <Layout
@@ -43,19 +42,41 @@ export default function SteeringCommittee() {
         "https://img.freepik.com/foto-gratis/reunion-departamento_23-2147626530.jpg?w=996&t=st=1681513108~exp=1681513708~hmac=eb17a71ffb76874c81f1199abbfe9cef4f0b1e4e4b7c07ea8c72ea9cc0604b0e"
       }
     >
-      <div className="lg:grid grid-cols-4 gap-8 py-10 px-5 lg:px-0 ">
-        {directives?.map((d) => {
-          return (
-            <div class=" max-w-sm mx-auto bg-white shadow-lg mb-10  ">
-              <Image src={image} alt={d.name} className="lg:h-32"  />
-              <div class="text-center py-4 lg:text-left lg: pl-2 bg-gray-100">
-                <p class="text-lg pb-3 text-black font-bold">{d.name}</p>
-                <p class="text-slate-500 font-medium">{d.position}</p>
-              </div>
+      {directives ? (
+        <div className="my-10">
+          <div className=" ml-[5%] flex flex-col items-center text-center ">
+            <div className=" w-[20%] bg-white shadow-lg items-center rounded-lg">
+              <Image src={image} className="h-24 "/>
+              <h1>{directives[0]?.name}</h1>
+              <h2>{directives[0]?.position}</h2>
             </div>
-          );
-        })}
-      </div>
+            <div className=" mt-8 w-[20%] bg-white shadow-lg rounded-lg">
+            <Image src={image} className="h-24"/>
+              <h1>{directives[1]?.name}</h1>
+              <h2>{directives[1]?.position}</h2>
+            </div>
+          </div>
+          <div className="flex justify-around">
+          <div className=" text-center mt-8 w-[20%] bg-white shadow-lg rounded-lg ml-[5%]">
+          <Image src={image} className="h-24"/>
+            <h1>{directives[2]?.name}</h1>
+            <h2>{directives[2]?.position}</h2>
+          </div>
+          <div className=" text-center mt-8 w-[20%] bg-white shadow-lg rounded-lg  ml-[5%]">
+          <Image src={image} className="h-24"/>
+            <h1>{directives[3]?.name}</h1>
+            <h2>{directives[3]?.position}</h2>
+          </div>
+          <div className=" text-center mt-8 w-[20%] bg-white shadow-lg rounded-lg ml-[5%]">
+          <Image src={image} className="h-24 "/>
+            <h1>{directives[4]?.name}</h1>
+            <h2>{directives[4]?.position}</h2>
+          </div>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
     </Layout>
   );
 }
