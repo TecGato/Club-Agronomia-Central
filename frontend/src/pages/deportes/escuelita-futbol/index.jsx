5dd1dc8c104b2db58deaad2ed15f3157c2bc65e9
DSinisterra <denissinisterra@gmail.com>
2023-04-19 11:46:01 -0500

    changes in navbar and entrenamiento arqueros page done

---
+++ frontend/src/pages/deportes/escuelita-futbol/index.jsx
@@ -0,0 +1,12 @@
+import { Layout } from '@/components/Page';
+
+export default function Escuelita() {
+    <Layout 
+        title='Escuelita de Fútbol'
+        image=''
+    >
+        <div  className="m-4">
+
+        </div>
+    </Layout>
+}
