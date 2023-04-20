f3579d870a59d2353be47559801abebb5ad7fab0
Luis Landaeta <luis.landaeta24@gmail.com>
2023-04-20 14:05:02 -0400

    final changes cloudinary backend

--- backend/routes/uploads.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const { uploadFile } = require('../controllers/uploads/postUploads');
-
-const router = Router();
-
-router.post('/', uploadFile);
-
-module.exports = router;
