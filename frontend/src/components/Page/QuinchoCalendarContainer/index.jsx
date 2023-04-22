f347c8feb46fe2a8abc70cfba87a40a67b0bea60
Elvis Franco <116116251+TecGato@users.noreply.github.com>
2023-04-21 23:07:00 -0400

    Start to work with Quincho view

--- frontend/src/components/Page/QuinchoCalendarContainer/index.jsx
+++ frontend/src/components/Page/QuinchoCalendarContainer/index.jsx
@@ -5,169 +5,135 @@ import { QuinchoCalendarForm } from '../QuinchoCalendarForm';
 import { QuinchoCardDetail } from '../QuinchoCardDetail';
 
 export function QuinchoCalendarContainer({ props }) {
+  console.log(props);
+  const events = props.data;
 
+  //const eventos = currentEventos;
 
-    const events = props.data;
+  const months = ['enero', 'febrero', 'marzo', 'abril'];
 
+  const eventDates = props.dataDates;
 
-    //const eventos = currentEventos;
+  //Declare an initial state of the form modal
+  const [showModal, setShowModal] = useState(false);
 
-    const months=[
-        "enero",
-        "febrero",
-        "marzo",
-        "abril"
-    ]
+  //Declare an initial state of the card detail
+  const [showCard, setShowCard] = useState(false);
 
+  return (
+    <Layout title={'Disponibilidad Quincho'} image={''}>
+      <div>
+        <div class="flex items-center mt-4">
+          <div class="flex ml-6">
+            <button>
+              <svg
+                class="w-6 h-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M15 19l-7-7 7-7"
+                />
+              </svg>
+            </button>
+            <button>
+              <svg
+                class="w-6 h-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M9 5l7 7-7 7"
+                />
+              </svg>
+            </button>
+          </div>
+          <h2 class="ml-2 text-xl font-bold leading-none">Mayo, 2023</h2>
+        </div>
 
-    const eventDates=props.dataDates;
+        {/* Encabezado del calendario mensual */}
+        <div class="grid grid-cols-7 mt-4 border-t-4 border-indigo-500">
+          <div class="pl-1 text-sm">Lunes</div>
+          <div class="pl-1 text-sm">Martes</div>
+          <div class="pl-1 text-sm">Miércoles</div>
+          <div class="pl-1 text-sm">Jueves</div>
+          <div class="pl-1 text-sm">Viernes</div>
+          <div class="pl-1 text-sm">Sábado</div>
+          <div class="pl-1 text-sm">Domingo</div>
+        </div>
 
+        {/* Contenedor principal */}
 
-    //Declare an initial state of the form modal
-    const [showModal, setShowModal] = useState(false);
+        <div class="grid flex-grow w-full h-auto grid-cols-7 grid-rows-5 gap-px pt-px mt-1 bg-gray-200">
+          {eventDates.map((dateEv) => {
+            return dateEv.date !== '2023-05-25' &&
+              dateEv.date !== '2023-05-29' &&
+              dateEv.date !== '2023-05-01' ? (
+              <QuinchoCalendarDay
+                key={dateEv.date}
+                dateCard={dateEv.date}
+                eventArray={events.filter(
+                  (dateE) => dateE.date === dateEv.date + 'T00:00:00.000Z'
+                )}
+                showCard={showCard}
+                setShowCard={setShowCard}
+              />
+            ) : null;
+          })}
+        </div>
 
-    //Declare an initial state of the card detail
-    const [showCard, setShowCard] = useState(false);
+        {/* Tags */}
 
-    return (
-        <Layout
-            title={'Disponibilidad Quincho'}
-            image={
-                ''
-            }
+        <h3>Indicadores de la reserva</h3>
+        <button class="flex items-center flex-shrink-0 h-5 px-1 text-xs">
+          <span class="flex-shrink-0 w-2 h-2 bg-gray-500 rounded-full"></span>
+          <span class="ml-2 font-medium leading-none truncate">
+            Reservas solicitadas
+          </span>
+        </button>
+
+        <button class="flex items-center flex-shrink-0 h-5 px-1 text-xs ">
+          <span class="flex-shrink-0 w-2 h-2 bg-yellow-500 rounded-full"></span>
+          <span class="ml-2 font-medium leading-none truncate">
+            Reservas Solicitadas y confirmadas
+          </span>
+        </button>
+
+        <button class="flex items-center flex-shrink-0 h-5 px-1 text-xs ">
+          <span class="flex-shrink-0 w-2 h-2 bg-green-500 rounded-full"></span>
+          <span class="ml-2 font-medium leading-none truncate">
+            Reservas Solicitadas, confirmadas y pagadas
+          </span>
+        </button>
+
+        {/* MODAL */}
+
+        <button
+          className="bg-blue-200 text-black active:bg-blue-500 font-bold px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
+          type="button"
+          onClick={() => setShowModal(true)}
         >
-            <div >
-                <div class="flex items-center mt-4">
-                    <div class="flex ml-6">
-                        <button>
-                            <svg
-                                class="w-6 h-6"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                            >
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    stroke-width="2"
-                                    d="M15 19l-7-7 7-7"
-                                />
-                            </svg>
-                        </button>
-                        <button>
-                            <svg
-                                class="w-6 h-6"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                            >
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    stroke-width="2"
-                                    d="M9 5l7 7-7 7"
-                                />
-                            </svg>
-                        </button>
-                    </div>
-                    <h2 class="ml-2 text-xl font-bold leading-none">
-                        Mayo, 2023
-                    </h2>
-                </div>
+          Solicitar nueva reserva
+        </button>
 
-                {/* Encabezado del calendario mensual */}
-                <div class="grid grid-cols-7 mt-4 border-t-4 border-indigo-500">
-                    <div class="pl-1 text-sm">Lunes</div>
-                    <div class="pl-1 text-sm">Martes</div>
-                    <div class="pl-1 text-sm">Miércoles</div>
-                    <div class="pl-1 text-sm">Jueves</div>
-                    <div class="pl-1 text-sm">Viernes</div>
-                    <div class="pl-1 text-sm">Sábado</div>
-                    <div class="pl-1 text-sm">Domingo</div>
-                </div>
+        {/* Si el estado del modal es true, renderiza el formulario */}
+        {showModal ? <QuinchoCalendarForm setShowModal={setShowModal} /> : null}
 
-                {/* Contenedor principal */}
-
-                <div class="grid flex-grow w-full h-auto grid-cols-7 grid-rows-5 gap-px pt-px mt-1 bg-gray-200">
-                    {eventDates.map((dateEv) => {
-                        return (
-                            dateEv.date!=="2023-05-25"&&dateEv.date!=="2023-05-29"&&dateEv.date!=="2023-05-01"?
-                            <QuinchoCalendarDay
-                                key={dateEv.date}
-                                dateCard={dateEv.date}
-                                eventArray={events.filter(
-                                    (dateE) => dateE.date === dateEv.date + "T00:00:00.000Z"
-                                )}
-                                showCard={showCard}
-                                setShowCard={setShowCard}
-                            />:null
-                        );
-                    })}
-                </div>
-
-                {/* Tags */}
-
-                <h3>Indicadores de la reserva</h3>
-                <button class="flex items-center flex-shrink-0 h-5 px-1 text-xs">
-                    <span class="flex-shrink-0 w-2 h-2 bg-gray-500 rounded-full"></span>
-                    <span class="ml-2 font-medium leading-none truncate">
-                        Reservas solicitadas
-                    </span>
-                </button>
-
-                <button class="flex items-center flex-shrink-0 h-5 px-1 text-xs ">
-                    <span class="flex-shrink-0 w-2 h-2 bg-yellow-500 rounded-full"></span>
-                    <span class="ml-2 font-medium leading-none truncate">
-                        Reservas Solicitadas y confirmadas
-                    </span>
-                </button>
-
-                <button class="flex items-center flex-shrink-0 h-5 px-1 text-xs ">
-                    <span class="flex-shrink-0 w-2 h-2 bg-green-500 rounded-full"></span>
-                    <span class="ml-2 font-medium leading-none truncate">
-                        Reservas Solicitadas, confirmadas y pagadas
-                    </span>
-                </button>
-
-                {/* MODAL */}
-
-                <button
-                    className="bg-blue-200 text-black active:bg-blue-500 font-bold px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
-                    type="button"
-                    onClick={() => setShowModal(true)}
-                >
-                    Solicitar nueva reserva
-                </button>
-
-                {/* Si el estado del modal es true, renderiza el formulario */}
-                {showModal ? (
-                <QuinchoCalendarForm
-
-                        setShowModal={setShowModal}
-
-                />
-                ) : null}
-
-                {/* Si el estado del card detail es true, renderiza card detail */}
-                {showCard?(
-                <QuinchoCardDetail
-                    showCard={showCard}
-                    setShowCard={setShowCard}
-                />
-                ):null
-
-                }
-
-            </div>
-        </Layout>
-
-    );
+        {/* Si el estado del card detail es true, renderiza card detail */}
+        {showCard ? (
+          <QuinchoCardDetail showCard={showCard} setShowCard={setShowCard} />
+        ) : null}
+      </div>
+    </Layout>
+  );
 }
-
-
-
-
-
-
--- frontend/src/pages/club/quincho/index.jsx
+++ frontend/src/pages/club/quincho/index.jsx
@@ -1,39 +1,24 @@
 import { QuinchoCalendarContainer } from '@/components/Page';
 
-
-
-
 export default function Quincho(props) {
-
-    return (
-
-    <QuinchoCalendarContainer
-        props= {props}
-    />
-    );
+  return <QuinchoCalendarContainer props={props} />;
 }
 
+export async function getStaticProps() {
+  try {
+    const res = await fetch('http://localhost:3001/api/events');
+    const data = await res.json();
 
+    const resDates = await fetch('http://localhost:3001/api/dateevents');
+    const dataDates = await resDates.json();
 
-export async function getStaticProps(){
-    try{
-
-        const res = await fetch('http://localhost:3001/api/events');
-        const data = await res.json();
-
-
-        const resDates = await fetch('http://localhost:3001/api/dateevents');
-        const dataDates = await resDates.json();
-
-        return {
-            props: {
-                data: data,
-                dataDates:dataDates,
-             }
-        }
-
-    }catch(error){
-        return{error:error.message}
-    }
-
+    return {
+      props: {
+        data: data,
+        dataDates: dataDates,
+      },
+    };
+  } catch (error) {
+    return { error: error.message };
+  }
 }
