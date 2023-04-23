54e4d31072d07e5be4e7a7c89e1f2f34fd10667b
Luis Landaeta <luis.landaeta24@gmail.com>
2023-04-23 18:59:54 -0400

    favicon changed for logo's png version

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -2,20 +2,17 @@ import Head from 'next/head';
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-    return (
-        <>
-            <Head>
-                <meta
-                    name="description"
-                    content="Club Deportido del barrio para el barrio"
-                />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1"
-                />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <Component {...pageProps} />
-        </>
-    );
+  return (
+    <>
+      <Head>
+        <meta
+          name="description"
+          content="Club Deportido del barrio para el barrio"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/logo.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
