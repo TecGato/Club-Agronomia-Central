9d5c838fd2e663c757071d35918a6d9f05095f9e
DSinisterra <denissinisterra@gmail.com>
2023-04-23 20:22:36 -0500

    link to noticias and testimonial by id

--- frontend/src/pages/noticias/[id]/index.jsx
+++ frontend/src/pages/noticias/[id]/index.jsx
@@ -7,7 +7,10 @@ export default function Post({news}) {
     const { id } = router.query;
 
     return (
-        <Layout title={'Noticias'} image={'https://img.freepik.com/foto-gratis/joven-corredor-atar-cordones_1421-46.jpg?size=626&ext=jpg&ga=GA1.2.1228025790.1681327921&semt=sph'}>
+        <Layout 
+            title={'Noticias'} 
+            image={'https://img.freepik.com/foto-gratis/joven-corredor-atar-cordones_1421-46.jpg?size=626&ext=jpg&ga=GA1.2.1228025790.1681327921&semt=sph'}
+        >
             <div className="m-4">
                 {/* <img
                     src={news.picture}
--- frontend/src/pages/testimonios/[id]/index.jsx
+++ frontend/src/pages/testimonios/[id]/index.jsx
@@ -4,7 +4,8 @@ export default function Testimony() {
     return (
         <Layout 
             title={'Testimonio'} 
-            image={'https://img.freepik.com/foto-gratis/joven-corredor-atar-cordones_1421-46.jpg?size=626&ext=jpg&ga=GA1.2.1228025790.1681327921&semt=sph'}>
+            image={'https://img.freepik.com/foto-gratis/joven-corredor-atar-cordones_1421-46.jpg?size=626&ext=jpg&ga=GA1.2.1228025790.1681327921&semt=sph'}
+        >
             <div className="m-4">
                 {/* ---------------------- POST ----------------------- */}
                 {/* {`644452a38eb617c5884c4fa9`} */}
