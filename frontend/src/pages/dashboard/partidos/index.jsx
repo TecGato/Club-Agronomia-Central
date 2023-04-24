dffe8d79e8a08d409966d9bb6fbdb084cc3bb1e5
AndresSalazar <andres.f.salazar@correounivalle.edu.co>
2023-04-24 10:11:53 -0500

    Matches table component on Dashboard

--- frontend/src/pages/dashboard/partidos/index.jsx
+++ frontend/src/pages/dashboard/partidos/index.jsx
@@ -1,9 +1,31 @@
 import { Layout } from '@/components/Dashboard';
+import { MatchesTable } from '@/components/Dashboard/MatchesTable';
 
-export default function Matches() {
+export default function Matches({data}) {
     return (
         <Layout>
-            <div>Matches</div>
+            <div>
+                <MatchesTable
+                    data={data}
+                />
+            </div>
         </Layout>
     );
 }
+
+
+
+export async function getStaticProps() {
+    try {
+    //   const resd = await fetch('http://ec2-3-15-46-181.us-east-2.compute.amazonaws.com:3001/api/events');
+      const res = await fetch('http://localhost:3001/api/matches');
+      const data = await res.json();
+
+
+      return {
+        props: { data: data },
+      };
+    } catch (error) {
+      return { error: error.message };
+    }
+  }
