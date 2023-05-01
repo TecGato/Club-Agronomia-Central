075f57b03bba9850fa2e028044a5ae0ddd14f51e
AndresSalazar <andres.f.salazar@correounivalle.edu.co>
2023-05-01 17:41:12 -0500

    handlers on delete and modify

--- frontend/src/hooks/useMatches.js
+++ frontend/src/hooks/useMatches.js
@@ -13,9 +13,33 @@ export function useMatches(){
         }
     };
 
+    const handlerModify = async (match) => {
+        try {
+            const { data } = await axios.put(
+            `http://localhost:3001/api/matches/${match.id}`,
+            match
+            );
+            return data;
+        } catch (error) {
+            throw new Error(error.message)
+        }
+        };
+
+
+    const handlerDelete = async (id) => {
+        try {
+            const { data } = await axios.delete(
+                `http://localhost:3001/api/matches/${id}`
+            );
+        } catch (error) {
+            throw new Error(error.message)
+        }
+        };
+
+
     return{
         handlerCreate,
-        // handlerModify,
-        // handlerDelete,
+        handlerModify,
+        handlerDelete,
     };
 }
