bcf458bf029f7282815b43bbfe24ccd877ecbc2d
AndresSalazar <andres.f.salazar@correounivalle.edu.co>
2023-05-02 11:57:31 -0500

    delete action implemented

--- frontend/src/hooks/useMatches.js
+++ frontend/src/hooks/useMatches.js
@@ -1,6 +1,13 @@
 import axios from 'axios';
+import { useState, useContext } from 'react';
+import useStore from '@/store/globalstore';
+import AppContext from '../../contexts/AppContext';
 
 export function useMatches(){
+
+    const { matches, setMatches } = useStore();
+
+    const [loading, setLoading] = useState(false);
 
     const handlerCreate = async (match)=>{
         try{
@@ -26,10 +33,10 @@ export function useMatches(){
         };
 
 
-    const handlerDelete = async (id) => {
+    const handlerDelete = async (_id) => {
         try {
             const { data } = await axios.delete(
-                `http://localhost:3001/api/matches/${id}`
+                `http://localhost:3001/api/matches/${_id}`
             );
         } catch (error) {
             throw new Error(error.message)
@@ -37,9 +44,20 @@ export function useMatches(){
         };
 
 
+    const getMatches = async ()=>{
+        if(matches.length===0){
+            setLoading(true);
+            await setMatches();
+            setLoading(false);
+        }
+    };
+
     return{
         handlerCreate,
         handlerModify,
         handlerDelete,
+        getMatches,
+        loading,
+        setLoading,
     };
 }
