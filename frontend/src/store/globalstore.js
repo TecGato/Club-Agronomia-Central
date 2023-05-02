8e92a17aca4118687c5fb62c44b759efd7614744
ARGEEK1 <antonio198889@gmail.com>
2023-05-02 13:03:32 +0200

    [UPDATE] athletes from global state

--- frontend/src/store/globalstore.js
+++ frontend/src/store/globalstore.js
@@ -8,7 +8,7 @@ const useStore = create((set) => ({
     const { data } = await axios.get(
       'http://ec2-3-15-46-181.us-east-2.compute.amazonaws.com:3001/api/athletes'
     );
-    set((state) => ({ ...state, atletas: data }));
+    set((state) => ({ ...state, atletas: data.reverse() }));
   },
   setReservas: async () => {
     const { data } = await axios.get(
@@ -22,6 +22,12 @@ const useStore = create((set) => ({
     );
     set((state) => ({ ...state, directiva: data }));
   },
+
+  modifyAtletas: (data) =>
+    set((state) => ({
+      ...state,
+      atletas: data,
+    })),
 }));
 
 export default useStore;
