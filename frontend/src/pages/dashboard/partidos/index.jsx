36c507338a74b991e86e2f2352f2705fbebb6ced
AndresSalazar <andres.f.salazar@correounivalle.edu.co>
2023-05-02 09:56:17 -0500

    overflow issue done

--- frontend/src/pages/dashboard/partidos/index.jsx
+++ frontend/src/pages/dashboard/partidos/index.jsx
@@ -111,51 +111,7 @@ export default function Matches({data}) {
       ) : null}
     </Layout>
   );
-    return (
-        <Layout>
-            <div>
 
-
-
-            <section
-            className={`w-full min-h-screen flex flex-col justify-self-center justify-center items-center xl:w-11/12 sm:w-5/6 `}
-            >
-                <article class="flex items-center mb-2">
-                <h1 className="text-5xl font-bold text-center">Encuentros del Club</h1>
-                    <button
-                        className="bg-blue-900 text-white active:bg-blue-500 font-bold px-6 py-1 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ml-3"
-                        type="button"
-                        onClick={() => setShowModalMatches(true)}
-                        >
-                    Crear nuevo encuentro
-                    </button>
-                </article>
-                <article className="flex flex-col gap-2 border bg-white border-neutral-300 shadow-[0px_3px_10px_2px_rgb(0_0_0_/_13%)] px-4 py-2 rounded-lg max-w-full h-2/3">
-                <DataGrid
-                    rows={rows}
-                    getRowId={(row) => row._id}
-                    columns={columns}
-                    slots={{
-                    toolbar: GridToolbar,
-                    }}
-                    paginationModel={paginationModel}
-                    onPaginationModelChange={setPaginationModel}
-                    pageSizeOptions={[5, 10, 25]}
-                    checkboxSelection={false}
-                    // onRowSelectionModelChange={onEdit}
-                    // disableMultipleRowSelection
-                />
-                </article>
-            </section>
-            </div>
-            {showModalMatches?(
-                <MatchesForm
-                    showModalMatches={showModalMatches}
-                    setShowModalMatches={setShowModalMatches}
-                />
-            ):null}
-        </Layout>
-    );
 }
 
 export async function getStaticProps() {
