04b7127223c82dfa598367e2f442b6bc4d35a507
Elvis Franco <116116251+TecGato@users.noreply.github.com>
2023-05-03 17:47:02 -0400

    [FiX] error in JWT

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -16,7 +16,7 @@ export default function App({ Component, pageProps }) {
       <Head>
         <meta
           name="description"
-          content="Club Deportido del barrio para el barrio"
+          content="Club Deportivo del barrio para el barrio"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/logo.png" />
