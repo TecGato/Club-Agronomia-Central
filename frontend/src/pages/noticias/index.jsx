5ebcf272b0365b2d4b0c1b6893e19059c95a4b96
Bryan Ismael Figueroa Quiroz <bryalquiroz@gmail.com>
2023-05-03 08:18:51 -0500

    fixes to deploy

--- frontend/src/pages/noticias/index.jsx
+++ frontend/src/pages/noticias/index.jsx
@@ -105,8 +105,7 @@ export default function Posts({ posts }) {
 
 export async function getStaticProps() {
   const res = await fetch(
-    // 'https://club-agronomia-central-production.up.railway.app/api/posts'
-    'http://localhost:3001/api/posts'
+    'https://club-agronomia-central-production.up.railway.app/api/posts'
   );
   const posts = await res.json();
   return {
