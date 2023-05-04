600e352dcf363a622e4d848674f9bff840e9c723
Bryan Ismael Figueroa Quiroz <bryalquiroz@gmail.com>
2023-05-03 19:43:14 -0500

    porfavor sirve

--- frontend/src/pages/auth/login/index.jsx
+++ frontend/src/pages/auth/login/index.jsx
@@ -19,7 +19,7 @@ export default function Login() {
       credentials: 'include',
     });
     if (response.status === 200) {
-      document.cookie = response.data.serialized
+      // document.cookie = response.data.serialized
       router.push('/dashboard');
     }
   };
