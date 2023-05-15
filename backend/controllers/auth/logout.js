c1c26c3a919d4f779f3f187d1043ac373cb4c74a
Elvis Franco <116116251+TecGato@users.noreply.github.com>
2023-05-15 11:56:18 -0400

    [fix] cookies

--- backend/controllers/auth/logout.js
+++ backend/controllers/auth/logout.js
@@ -3,15 +3,11 @@ const { serialize } = require('cookie');
 
 const logout = async (req = request, res = response) => {
   try {
-    const serialized = serialize('authToken', null, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
+    res.cookie('authToken', null, {
+      secure: true,
       sameSite: 'none',
       maxAge: 0,
-      path: '/',
     });
-
-    res.cookie(serialized);
 
     return res.status(200).json({
       msg: 'Logged out Successfully',
